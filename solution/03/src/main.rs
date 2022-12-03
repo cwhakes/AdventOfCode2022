ca5939f806c9a6584b3f17446ff53096febf01d0
Will Hakes <info@cwilliamhakes.com>
2022-12-02 22:22:06 -0700

    Day 03

--- solution/03/src/main.rs
+++ solution/03/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fmt::Display;
 use std::fs::File;
 use std::io::Read;
@@ -15,11 +16,40 @@ fn main() {
 }
 
 fn get_answer1(input: &str) -> impl Display {
-	let _ = input;
-	0
+	let mut acc = 0;
+	for rucksack in input.lines() {
+		let (a, b) = rucksack.split_at(rucksack.len() / 2);
+		let hash_a: HashSet<_> = a.chars().collect();
+		let hash_b: HashSet<_> = b.chars().collect();
+		for a in hash_a {
+			if hash_b.contains(&a) {
+				if a.is_ascii_uppercase() {
+					acc += a as u32 - 64 + 26
+				} else {
+					acc += a as u32 - 96
+				}
+			}
+		}
+	}
+	acc
 }
 
 fn get_answer2(input: &str) -> impl Display {
-	let _ = input;
-	0
+	let mut acc = 0;
+	let mut iter = input.lines();
+	while let [Some(a), Some(b), Some(c)] = [iter.next(), iter.next(), iter.next()] {
+		let hash_a: HashSet<_> = a.chars().collect();
+		let hash_b: HashSet<_> = b.chars().collect();
+		let hash_c: HashSet<_> = c.chars().collect();
+		for a in hash_a {
+			if hash_b.contains(&a) && hash_c.contains(&a) {
+				if a.is_ascii_uppercase() {
+					acc += a as u32 - 64 + 26
+				} else {
+					acc += a as u32 - 96
+				}
+			}
+		}
+	}
+	acc
 }
