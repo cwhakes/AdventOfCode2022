d91d420fa2e2335628a3fea670c89ff5a10c38b0
C William Hakes <info@cwilliamhakes.com>
2022-12-21 23:57:18 -0700

    Day 22.1

--- solution/22/src/main.rs
+++ solution/22/src/main.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::fmt::Display;
 
-static INPUT: &str = include_str!("../../../input/01/input");
+static INPUT: &str = include_str!("../../../input/22/input");
 
 fn main() {
 	let answer = get_answer1(INPUT);
@@ -11,13 +12,218 @@ fn main() {
 }
 
 fn get_answer1(input: &str) -> impl Display {
-	let _ = input;
-	0
+	let (map, path) = input.split_once("\n\n").unwrap();
+	let map = Map::new(map);
+	let path = Path::new(path);
+
+	let mut cursor = Cursor::new(&map);
+	cursor.walk_all(path.0, &map);
+	cursor.score()
 }
 
 fn get_answer2(input: &str) -> impl Display {
 	let _ = input;
 	0
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Tile {
+	Space,
+	Wall,
+}
+
+struct Map(HashMap<(i32, i32), Tile>);
+
+impl Map {
+	fn new(input: &str) -> Self {
+		let mut map = HashMap::new();
+		for (y, line) in input.lines().enumerate() {
+			for (x, tile) in line.bytes().enumerate() {
+				match tile {
+					b'.' => {map.insert((x as i32, y as i32),  Tile::Space);},
+					b'#' => {map.insert((x as i32, y as i32),  Tile::Wall);},
+					_ => {},
+				}
+			}
+		}
+		Self(map)
+ 	}
+}
+
+#[derive(Debug, Clone)]
+enum Inst {
+	Forward(i32),
+	Left,
+	Right,
+}
+
+#[derive(Debug, Clone)]
+struct Path(Vec<Inst>);
+
+impl Path {
+	fn new(input: &str) -> Self {
+		use nom::branch::alt;
+		use nom::bytes::complete::tag;
+		use nom::character::complete::i32 as parse_i32;
+		use nom::combinator::{map, value};
+		use nom::multi::many0;
+		use nom::IResult;
+
+		let mut parser = many0(alt((map(parse_i32, Inst::Forward), value(Inst::Left, tag("L")), value(Inst::Right, tag("R")))));
+		let parsed: IResult<_, _> = parser(input);
+		let (_, new) = parsed.unwrap();	
+		Self(new)
+	}
+}
+
+#[derive(Debug, Default)]
+struct Cursor {
+	loc: (i32, i32),
+	dir: u8,
+	min_x: HashMap<i32, (i32, i32)>,
+	max_x: HashMap<i32, (i32, i32)>,
+	min_y: HashMap<i32, (i32, i32)>,
+	max_y: HashMap<i32, (i32, i32)>,
+}
+
+impl Cursor {
+	fn new(map: &Map) -> Self {
+		let (&loc, _) = map.0.iter().filter(|((_, y), t)| *y == 0 && **t == Tile::Space).min_by_key(|((x, _), _)| x).unwrap();
+		Self {
+			loc,
+			dir: 0,
+			..Self::default()
+		}
+	}
+
+	fn score(&self) -> i32 {
+		1000 * (self.loc.1 + 1) + 4 * (self.loc.0 + 1) + self.dir as i32
+	}
+
+	fn walk(&mut self, inst: Inst, map: &Map) {
+		match inst {
+			Inst::Left => self.dir = (self.dir + 3) % 4,
+			Inst::Right => self.dir = (self.dir + 1) % 4,
+			Inst::Forward(num) => {
+				for _ in 0..num {
+					let mut new_loc;
+					let new_tile = match self.dir {
+						0 => {
+							new_loc = (self.loc.0 + 1, self.loc.1);
+							map.0.get(&new_loc).or_else(|| {
+								new_loc = *self.min_x.entry(self.loc.1).or_insert_with(|| {
+									*map.0.keys().filter(|(_, y)| *y == self.loc.1).min_by_key(|(x, _)| x).unwrap()
+								});
+								map.0.get(&new_loc)
+							}).unwrap()
+						}
+						1 => {
+							new_loc = (self.loc.0, self.loc.1 + 1);
+							map.0.get(&new_loc).or_else(|| {
+								new_loc = *self.min_y.entry(self.loc.0).or_insert_with(|| {
+									*map.0.keys().filter(|(x, _)| *x == self.loc.0).min_by_key(|(_, y)| y).unwrap()
+								});
+								map.0.get(&new_loc)
+							}).unwrap()
+						}
+						2 => {
+							new_loc = (self.loc.0 - 1, self.loc.1);
+							map.0.get(&new_loc).or_else(|| {
+								new_loc = *self.max_x.entry(self.loc.1).or_insert_with(|| {
+									*map.0.keys().filter(|(_, y)| *y == self.loc.1).max_by_key(|(x, _)| x).unwrap()
+								});
+								map.0.get(&new_loc)
+							}).unwrap()
+						}
+						3 => {
+							new_loc = (self.loc.0, self.loc.1 - 1);
+							map.0.get(&new_loc).or_else(|| {
+								new_loc = *self.max_y.entry(self.loc.0).or_insert_with(|| {
+									*map.0.keys().filter(|(x, _)| *x == self.loc.0).max_by_key(|(_, y)| y).unwrap()
+								});
+								map.0.get(&new_loc)
+							}).unwrap()
+						}
+						_ => panic!()
+					};
+					if Tile::Space == *new_tile {
+						self.loc = new_loc;
+					} else {
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	fn walk_all(&mut self, insts: impl IntoIterator<Item = Inst>, map: &Map) {
+		for inst in insts {
+			self.walk(inst, map);
+		}
+	}
+
+	fn walk2(&mut self, inst: Inst, map: &Map) {
+		match inst {
+			Inst::Left => self.dir = (self.dir + 3) % 4,
+			Inst::Right => self.dir = (self.dir + 1) % 4,
+			Inst::Forward(num) => {
+				for _ in 0..num {
+					let mut new_loc;
+					let mut new_dir = self.dir;
+					let new_tile = match self.dir {
+						0 => {
+							new_loc = (self.loc.0 + 1, self.loc.1);
+							map.0.get(&new_loc).or_else(|| {
+								new_loc = *self.min_x.entry(self.loc.1).or_insert_with(|| {
+									*map.0.keys().filter(|(_, y)| *y == self.loc.1).min_by_key(|(x, _)| x).unwrap()
+								});
+								map.0.get(&new_loc)
+							}).unwrap()
+						}
+						1 => {
+							new_loc = (self.loc.0, self.loc.1 + 1);
+							map.0.get(&new_loc).or_else(|| {
+								new_loc = *self.min_y.entry(self.loc.0).or_insert_with(|| {
+									*map.0.keys().filter(|(x, _)| *x == self.loc.0).min_by_key(|(_, y)| y).unwrap()
+								});
+								map.0.get(&new_loc)
+							}).unwrap()
+						}
+						2 => {
+							new_loc = (self.loc.0 - 1, self.loc.1);
+							map.0.get(&new_loc).or_else(|| {
+								new_loc = *self.max_x.entry(self.loc.1).or_insert_with(|| {
+									*map.0.keys().filter(|(_, y)| *y == self.loc.1).max_by_key(|(x, _)| x).unwrap()
+								});
+								map.0.get(&new_loc)
+							}).unwrap()
+						}
+						3 => {
+							new_loc = (self.loc.0, self.loc.1 - 1);
+							map.0.get(&new_loc).or_else(|| {
+								new_loc = *self.max_y.entry(self.loc.0).or_insert_with(|| {
+									*map.0.keys().filter(|(x, _)| *x == self.loc.0).max_by_key(|(_, y)| y).unwrap()
+								});
+								map.0.get(&new_loc)
+							}).unwrap()
+						}
+						_ => panic!()
+					};
+					if Tile::Space == *new_tile {
+						self.loc = new_loc;
+					} else {
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	fn walk_all2(&mut self, insts: impl IntoIterator<Item = Inst>, map: &Map) {
+		for inst in insts {
+			self.walk(inst, map);
+		}
+	}
 }
 
 #[cfg(test)]
@@ -27,7 +233,7 @@ mod test {
 
 	#[test]
 	fn part1() {
-		assert_eq!("0", &get_answer1(TEST).to_string());
+		assert_eq!("6032", &get_answer1(TEST).to_string());
 		// assert_eq!("0", &get_answer1(INPUT).to_string());
 	}
 
